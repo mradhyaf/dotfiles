47dbfff187da3fc0daea342df384aaa91aa641a7
mradhyaf <mradhyaf@gmail.com>
2024-12-03 10:34:01 +0700

    stuff

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,12 +1,29 @@
 return {
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        'sainnhe/everforest',
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme rose-pine]])
-        end,
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.everforest_enable_italic = true
+            vim.g.everforest_background = 'hard'
+            vim.cmd.set('background=light')
+            vim.cmd.colorscheme('everforest')
+        end
     }
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     lazy = false,
+    --     priority = 1000,
+    --     -- opts = {
+    --     --     variant = "dawn",
+    --     --     dark_variant = "moon",
+    --     -- },
+    --     config = function()
+    --         vim.cmd([[colorscheme rose-pine-dawn]])
+    --     end,
+    -- }
 }
 
